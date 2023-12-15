c3ff1dfe4d2369cbbb856e4c0c3207c5aa1cf80b
lriveraascencio <lriveraascencio@USQROLRIVERAAS1.us.deloitte.com>
2023-12-14 19:59:07 -0600

    JPA Base RestServices Development Completed

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into base values(101, 'kreddy@stacksimplify.com', 'Kalyan', 'Reddy', 'admin', 'ssn101', 'kreddy');
+insert into base values(102, 'gwiser@stacksimplify.com', 'Greg', 'Wiser', 'admin', 'ssn102', 'gwiser');
+insert into base values(103, 'dmark@stacksimplify.com', 'David', 'Mark', 'admin', 'ssn103', 'dmark');
